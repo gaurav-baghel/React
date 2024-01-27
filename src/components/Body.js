ad46519a05a8cafd352c6dfcd866bc96de0e234a
Your Name <gaurav.baghel@infoedge.com>
2024-01-27 23:11:07 +0530

    added routing to restaurant cards...........

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 // import restaurantList from "../utils/mockData";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 
 const Body = () =>{
@@ -19,12 +20,13 @@ const Body = () =>{
 
   const fetchData = async() => {
     const data = await fetch(
-      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4089123&lng=77.3177894&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      , {
-        headers: {
-        'x-cors-api-key': 'temp_b63fc4e2b79e4b8bb58b6b24e966c6a7'
-        }
-      }
+      // "https://proxy.cors.sh/
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4089123&lng=77.3177894&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      // , {
+      //   headers: {
+      //   'x-cors-api-key': 'temp_b63fc4e2b79e4b8bb58b6b24e966c6a7'
+      //   }
+      // }
     );
 
       const json = await data.json();
@@ -79,7 +81,7 @@ const Body = () =>{
             {
               filteredListRestaurant.map(
                 (restaurant) => (
-                  <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+                  <Link to={'/restaurant/'+restaurant.info.id} key={restaurant.info.id}><RestaurantCard resData={restaurant}/></Link>
                 )
               )
             }
