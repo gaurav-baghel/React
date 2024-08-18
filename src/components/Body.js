e94e2cb983d706f53c3a2893d6d3221dc52786a9
Gaurav Baghel <gaurav.baghel@Infoedge.com>
2024-08-18 15:37:50 +0530

    added cors proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,13 +24,12 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      // "https://proxy.cors.sh/
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.40980&lng=77.31000&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      // , {
-      //   headers: {
-      //   'x-cors-api-key': 'temp_b63fc4e2b79e4b8bb58b6b24e966c6a7'
-      //   }
-      // }
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.40980&lng=77.31000&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      , {
+        headers: {
+        'x-cors-api-key': 'temp_b63fc4e2b79e4b8bb58b6b24e966c6a7'
+        }
+      }
     );
 
     const json = await data.json();
